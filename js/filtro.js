583320eba90508d71e365b1b5dec08457ed1fe7d
Jose Guillermo <velasquezperezjg@gmail.com>
2023-09-14 20:00:03 -0500

    Se añadio un texto de contenido en un elemento section que dependerá de los cambios realizados en el select, esto se logró con un controlador de eventos "change", el archivo de estilos se modificó para agregar un borde solido al elemento section.

--- js/filtro.js
+++ js/filtro.js
@@ -1,3 +1,13 @@
 //Declaración de constantes que almacenan nodos por id.
-const input = document.querySelector("id","cartegorias");
+const selectInput = document.querySelector("#categorias");
 const output = document.getElementById("listas_productos");
+
+//Añadiendo un controlador de evento DOMContentLoaded en la ventana del navegador
+window.addEventListener('DOMContentLoaded', () => {
+    console.log('loaded')
+});
+
+//Añadiendo un controlador de evento en el elemento select con id "categorias"
+selectInput.addEventListener("change", e => {
+    output.textContent = `Has elegido la categoría ${e.target.value}`;
+});
